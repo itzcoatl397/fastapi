cd6d7eac768d5e34f7b75a2632c4cc4b90482fc8
itzcoatl397 <GerardoItzcoatl.21@outlook.com>
2026-01-28 00:20:17 -0600

    creamos el post y estamos aprendiendo el put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.params import Query
+from fastapi.params import Query, Body
 
 app = FastAPI()
 
@@ -76,13 +76,71 @@ def posts(query: str | None = Query(default=None, description="Query para obtene
 
 
 @app.get("/posts/{id}")
-def get_post(id: int):
-    data = {}
+def get_post(
+        id: int,
+        include_content: bool = Query(default=False, description="Incluir contenido completo")
+):
     for post in POSTS:
         if post["id"] == id:
-           return  {
-               "data": post,
-           }
+            if not include_content:
+                return {
+                    "id": post["id"],
+                    "title": post["title"],
+                }
+            return {
+                "data": post
+            }
+
     return {
-        "error":"Post not found",
+        "error": "Post not found"
     }
+
+
+@app.post("/posts")
+def create_post(post: dict = Body(...)):
+    if "title" not in post or "content" not in post:
+        return {"error", "Title  y Content son requerido"}
+
+    if not str(post["title"]).strip():
+        return {"error", "Title no puede estar vacio"}
+
+
+    new_id =(POSTS[-1]["id"] + 1) if post else 1
+    new_post = {
+        "id": new_id,
+        "title": post["title"],
+        "content": post["content"]
+    }
+    POSTS.append(new_post)
+
+    return {
+
+        "status_code": 201,
+        "message": "Post created successfully",
+        "data": new_post,
+    }
+
+
+
+@app.put("/posts/{id}")
+def update_post(
+        id: int,
+        postJson: dict = Body(...),
+):
+    if "title" not in postJson or "content" not in postJson:
+        return {"error", "Title  y Content son requerido"}
+
+    if not str(postJson["title"]).strip():
+        return {"error", "Title no puede estar vacio"}
+
+
+    for post in POSTS:
+        if post["id"] == id:
+
+            post["title"] = postJson["title"]
+            post["content"] = postJson["content"]
+
+
+
+
+
